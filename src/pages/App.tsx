24f6121f81f35a2d0fed6e8bbc5382cdcd6bb912
Gabrafo <gafagundesmesquita@gmail.com>
2025-11-21 08:48:04 -0300

    refactor: remove unused GoddessImage and Salutation components, and clean up related styles

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,23 +1,12 @@
 import "../styles/theme.css"
 import "../styles/global.css"
 
-import style from "./App.module.css"
-
-import melinoe from "../assets/melinoe.webp"
-
 import { Heading } from "../components/Heading"
-import { GoddessImage } from "../components/GoddessImage"
-import { Salutation } from "../components/Salutation"
 
 function App() {
   return (
     <>
-      <Heading />
-
-      <div className={style['goddess-container']}>
-        <GoddessImage src={melinoe} width={5} height={5} alt="Melinoe, goddess of ghosts" />
-        <Salutation>Melinoe says: death to Chronos!{/* The text is passing down as children of Salutation*/}</Salutation>
-      </div>
+      <Heading>Olá, mundo!</Heading>
     </>
   );
 }

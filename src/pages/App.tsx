30a3acf42777140927713521e38e489be50fd905
Gabrafo <gafagundesmesquita@gmail.com>
2025-11-22 14:15:19 -0300

    feat: basic HTML skeleton, which will not be used (replaced in the next commit)

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,18 +1,18 @@
 import "../styles/theme.css"
 import "../styles/global.css"
 
-import { Heading } from "../components/Heading"
-import { TimerIcon } from "lucide-react";
-
 function App() {
   return (
     <>
-      <Heading>
-        Olá, mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
+      <div className="container-fluid"> {/** Stretches the full width (used for customizing background) */}
+        <div className="container"> {/* Used for limiting width, and aligning to the center of the page */}
+          <div className="content">
+            <section>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi pariatur autem tenetur cupiditate! Veniam dolor saepe nam excepturi perferendis sit consequuntur voluptatum optio unde, reprehenderit illo quibusdam suscipit repudiandae similique.
+            </section>
+          </div>
+        </div>
+      </div>
     </>
   );
 }

6aa181a9aa212f89dd3aa8fc2223ad4330fe06b8
Gabrafo <gafagundesmesquita@gmail.com>
2025-11-22 14:31:35 -0300

    feat: implement Container component and refactor App layout

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,40 +1,14 @@
 import "../styles/theme.css"
 import "../styles/global.css"
+import { Container } from "../components/Container";
 
 function App() {
   return (
     <>
-      <div className="container"> {/* Used for limiting width, and aligning to the center of the page */}
-        <div className="content">
-          <section>
-            LOGO
-          </section>
-        </div>
-      </div>
-
-      <div className="container">
-        <div className="content">
-          <section>
-            MENU
-          </section>
-        </div>
-      </div>
-
-      <div className="container">
-        <div className="content">
-          <section>
-            FORM
-          </section>
-        </div>
-      </div>
-
-      <div className="container">
-        <div className="content">
-          <section>
-            FOOTER
-          </section>
-        </div>
-      </div>
+      <Container>Logo</Container>
+      <Container>Menu</Container>
+      <Container>Form</Container>
+      <Container>Footer</Container>
     </>
   );
 }
